7fee4a1757cdb94966ff0335668e249c901c1dbe
jonny rhea <jonathan.rhea@gmail.com>
2020-04-12 01:29:35 -0500

    mods made to update to the latest version of mothra

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-tmux new-session -d -s foo './../target/debug/imp --datadir ~/.imp --debug-level trace'
-tmux split-window -v -t 0 'sleep 2 && ./../target/debug/imp --boot-nodes $(cat ~/.imp/network/enr.dat) --port 9001 --datadir /tmp/.imp --debug-level trace'
+tmux new-session -d -s foo './../target/debug/imp mothra --datadir ~/.imp --debug-level trace'
+tmux split-window -v -t 0 'sleep 2 && ./../target/debug/imp mothra --boot-nodes $(cat ~/.imp/network/enr.dat) --port 9001 --datadir /tmp/.imp --debug-level trace'
 tmux select-layout tile
 tmux rename-window 'the dude abides'
 tmux attach-session -d
