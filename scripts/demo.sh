01c1b7b599fc091d30948ad59b408285d73a21a6
jonny rhea <jonathan.rhea@gmail.com>
2020-04-14 17:52:08 -0500

    P2PService responds to same SIGINT command as main() to shutdown

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tmux new-session -d -s foo './../target/debug/imp mothra --datadir ~/.imp --debug-level trace'
+tmux new-session -d -s foo './../sim/mock-node/target/debug/mock-node mothra --datadir ~/.imp --debug-level trace'
 tmux split-window -v -t 0 'sleep 2 && ./../target/debug/imp mothra --boot-nodes $(cat ~/.imp/network/enr.dat) --port 9001 --datadir /tmp/.imp --debug-level trace'
 tmux select-layout tile
 tmux rename-window 'the dude abides'
