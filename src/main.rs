520cb7d4b4776051efeeea9e336d0c3aec778ed2
jonny rhea <jonathan.rhea@gmail.com>
2020-04-16 00:21:41 -0500

    more logging work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-extern crate target_info;
-#[macro_use]
 extern crate error_chain;
+extern crate target_info;
 use clap::{App, Arg};
-use env_logger::Env;
-use slog::{debug, info, o, trace, warn, Drain, Level, Logger};
+use slog::{debug, info, o, trace, warn};
 use std::time::Duration;
 use tokio_01::prelude::future::Future;
 use tokio_02::sync::watch;
@@ -15,7 +13,7 @@ use network::NetworkService;
 use p2p::{cli_app, P2PService};
 
 const CLIENT_NAME: &str = "imp";
-const PROTOCOL_VERSION: &str = "imp/libp2p";
+const P2P_PROTOCOL_VERSION: &str = "imp/libp2p";
 
 fn main() -> Result<(), std::io::Error> {
     // Parse the CLI parameters.
@@ -23,6 +21,14 @@ fn main() -> Result<(), std::io::Error> {
         .version(clap::crate_version!())
         .author("Jonny Rhea")
         .about("Eth2 Network Agent")
+        .arg(
+            Arg::with_name("p2p-protocol-version")
+                .long("p2p-protocol-version")
+                .value_name("P2P_PROTOCOL_VERSION")
+                .help("P2P protocol version to advertise.")
+                .takes_value(true)
+                .default_value(P2P_PROTOCOL_VERSION),
+        )
         .arg(
             Arg::with_name("debug-level")
                 .long("debug-level")
@@ -35,46 +41,39 @@ fn main() -> Result<(), std::io::Error> {
         .subcommand(cli_app())
         .get_matches();
 
-    let debug_level = arg_matches
-        .value_of("debug-level")
-        .ok_or_else(|| "Expected --debug-level flag".to_string())
-        .unwrap();
+    let p2p_protocol_version = arg_matches.value_of("p2p-protocol-version").unwrap();
+
+    // default to this debug-level value.
+    // if mothra submommand has a specific debug-level,
+    // then mothra will use it
+    let debug_level = arg_matches.value_of("debug-level").unwrap();
 
     // configure logging
-    env_logger::Builder::from_env(Env::default()).init();
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build();
-    let drain = match debug_level {
-        "info" => drain.filter_level(Level::Info),
-        "debug" => drain.filter_level(Level::Debug),
-        "trace" => drain.filter_level(Level::Trace),
-        "warn" => drain.filter_level(Level::Warning),
-        "error" => drain.filter_level(Level::Error),
-        "crit" => drain.filter_level(Level::Critical),
-        _ => drain.filter_level(Level::Info),
-    };
-    let slog = Logger::root(drain.fuse(), o!());
-    let log = slog.new(o!("imp" => "imp"));
+    let slog = utils::config_logger(debug_level, true);
+    let log = slog.new(o!("imp" => ""));
 
     let client_name: String = CLIENT_NAME.into();
     let platform: String = format!("v{}", env!("CARGO_PKG_VERSION"));
-    let protocol_version: String = PROTOCOL_VERSION.into();
 
     let mut runtime = tokio_compat::runtime::Runtime::new()?;
+
+    info!(log, "Starting imp");
+
     let p2p_service = P2PService::new(
         &runtime.executor(),
         client_name,
         platform,
-        protocol_version,
+        p2p_protocol_version.into(),
         &arg_matches,
         log.new(o!("imp" => "P2PService")),
     );
-    let network_service = NetworkService::new();
-    let agent = Agent::new(network_service.clone());
+    let network_service = NetworkService::new(log.new(o!("imp" => "NetworkService")));
+    let agent = Agent::new(log.new(o!("imp" => "Agent")));
 
+    let (shutdown_tx, shutdown_rx) = watch::channel::<Message>(Message::None);
+
+    // main "event loop"
     runtime.block_on_std(async move {
-        let (shutdown_tx, shutdown_rx) = watch::channel::<Message>(Message::None);
         async move {
             let rx = shutdown_rx.clone();
             task::spawn(async move {
@@ -84,16 +83,15 @@ fn main() -> Result<(), std::io::Error> {
             agent.spawn(shutdown_rx).await;
         }
         .await;
-        // block the current thread until Ctrl+C is received.
+        // block the current thread until SIGINT is received.
         signal::ctrl_c().await.expect("failed to listen for event");
-        println!("Sending shutdown signal.");
-        let _ = shutdown_tx.broadcast(Message::Shutdown);
     });
+
+    info!(log, "Sending shutdown signal.");
+    let _ = shutdown_tx.broadcast(Message::Shutdown);
 
     // Shutdown the runtime
     runtime.shutdown_on_idle().wait().unwrap();
-
-    println!("Exiting imp.");
-
+    info!(log.clone(), "Exiting imp.");
     Ok(())
 }
