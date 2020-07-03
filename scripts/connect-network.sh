ba326e91de2083fa13051b1813b53d6324605cbd
jonny rhea <jonathan.rhea@gmail.com>
2020-07-03 11:48:50 -0500

    mothra needed to be started from a tokio-runtime in order for the network agent to subscribe and log gossip messages

--- scripts/connect-network.sh
+++ scripts/connect-network.sh
@@ -6,5 +6,5 @@ DATA_DIR=$HOME/.imp/datadir
 LISTEN_ADDRESS=$(ipconfig getifaddr en0)
 BOOTNODES=$(cat ~/.$NETWORK/enrs.txt | tr "\n" "," | sed -e "s/,$//g")
 mkdir -p $DATA_DIR
-RUST_BACKTRACE=full ./../target/debug/imp --debug-level info mothra --debug-level trace --boot-nodes $BOOTNODES --listen-address $LISTEN_ADDRESS --port 13001 --discovery-port 12001 --datadir $DATA_DIR
+RUST_BACKTRACE=full ./../target/debug/imp --debug-level trace mothra --debug-level trace --boot-nodes $BOOTNODES --listen-address $LISTEN_ADDRESS --port 13001 --discovery-port 12001 --datadir $DATA_DIR
 rm -rf $DATA_DIR
